57c8f7a7b864cac0b95ede5f6ab97b264a0aee5c
Yang Seok <didtjr5080@naver.com>
2025-10-18 23:28:09 +0900

    start

---
+++ main.py
@@ -0,0 +1,14 @@
+import module
+
+# module.make_key()
+
+# module.get_key()
+
+
+a = 'kyu'
+
+b = module.encoding(a)
+
+c = module.denoding(b)
+
+print(a==c)
---
+++ module.py
@@ -0,0 +1,29 @@
+from cryptography.fernet import Fernet
+
+def make_key():
+    # 키 생성
+    key = Fernet.generate_key()
+
+    with open('key', 'wb') as f:
+        f.write(key)
+        print('키 생성 완료')
+
+def get_key():
+    with open('key', 'rb') as f:
+        key = f.read()
+        return key
+
+def encoding(input_text):
+    # 암호화
+    cipher = Fernet(get_key())
+    plaintext = input_text
+    encrypted = cipher.encrypt(plaintext.encode())
+    print(f"Encrypted: {encrypted}")
+    return encrypted
+
+def denoding(encrypted):
+    # 복호화
+    cipher = Fernet(get_key())
+    decrypted = cipher.decrypt(encrypted).decode()
+    print(f"Decrypted: {decrypted}")
+    return decrypted
